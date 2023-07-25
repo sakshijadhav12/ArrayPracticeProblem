4e69f5d47c8d91ffb6f60d15fb830ae8e224f678
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-25 19:41:17 +0530

    [sakshi] practcing some problem on array

---
+++ ArrayPracticeProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrayPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PRACTICE PROBLEM");
+
+        }
+    }
+}
