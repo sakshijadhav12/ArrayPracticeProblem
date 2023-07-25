1963d753fd37e6ae65e55d0ddedeca4485e69148
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-25 20:54:15 +0530

    [sakshi] modifying program.cs to find out duplicate elements in array

---
+++ ArrayPracticeProblem/Duplicate Elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayPracticeProblem
+{
+    internal class Duplicate_Elements
+    {
+        public void CountDuplicateElements()
+        {
+            Console.WriteLine("Enter how many elements you want to store in the array:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int j;
+            int ctr = 0;
+            int[] arr = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"Enter element {i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                for (j = i + 1; j < num; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        ctr++;
+                        break; // Add this to avoid counting the same duplicate multiple times
+                    }
+                }
+            }
+
+            Console.WriteLine($"{ctr} duplicates found.");
+        }
+    }
+}
--- ArrayPracticeProblem/Program.cs
+++ ArrayPracticeProblem/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("PRACTICE PROBLEM");
+            Console.WriteLine("Select your option \n1.Duplicate Elements ");
+            int option = int.Parse(Console.ReadLine()); 
+            switch(option)
+            {
+                case 1:
+                    Duplicate_Elements ob = new Duplicate_Elements();
+                    ob.CountDuplicateElements(); 
+                    break;
+            }
 
         }
     }
